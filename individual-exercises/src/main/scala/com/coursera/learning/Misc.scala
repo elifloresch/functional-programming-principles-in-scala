f3d69e3a4f38530f055ce366202917b82a31a31c
elifloresch <elifloresch@gmail.com>
2017-05-23 21:56:28 +0200

    New exercises

---
+++ individual-exercises/src/main/scala/com/coursera/learning/Misc.scala
@@ -0,0 +1,32 @@
+package com.coursera.learning
+
+object Misc {
+  def sum(f: Int => Int)(a: Int, b: Int): Int = {
+    def loop(a: Int, acc: Int): Int = {
+      if (a > b) acc
+      else loop(a + 1, f(a) + acc)
+    }
+
+    loop(a, 0)
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(sum(x => x)(1, 5))
+    println(product(x => x)(1, 4))
+    println(fact(4))
+  }
+
+  def product(f: Int => Int)(a: Int, b: Int): Int = {
+    def loop(a: Int, acc: Int): Int = {
+      if (a > b) acc
+      else loop(a + 1, f(a) * acc)
+    }
+
+    loop(a, 1)
+  }
+
+
+  def fact(n: Int): Int = {
+    product(x => x)(1, n)
+  }
+}
