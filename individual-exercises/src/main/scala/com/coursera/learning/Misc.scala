f3737c46e2ce7824566e0b7fa6974e98d04b58ec
elifloresch <elifloresch@gmail.com>
2017-05-28 19:38:32 +0200

    adding more exercises

--- individual-exercises/src/main/scala/com/coursera/learning/Misc.scala
+++ individual-exercises/src/main/scala/com/coursera/learning/Misc.scala
@@ -1,33 +1,28 @@
 package com.coursera.learning
 
 object Misc {
-  def sum(f: Int => Int)(a: Int, b: Int): Int = {
-    def loop(a: Int, acc: Int): Int = {
-      if (a > b) acc
-      else loop(a + 1, f(a) + acc)
-    }
-
-    loop(a, 0)
-  }
 
   def main(args: Array[String]): Unit = {
     println(sum(x => x)(1, 5))
-    println(product(x => x)(1, 4))
-    println(fact(4))
+    println(product(x => x)(1, 5))
+    println(fact(5))
   }
 
-  def product(f: Int => Int)(a: Int, b: Int): Int = {
-    def loop(a: Int, acc: Int): Int = {
-      if (a > b) acc
-      else loop(a + 1, f(a) * acc)
-    }
+  def sum(f: Int => Int)(a: Int, b: Int): Int = mapReduce(f, (x, y) => x + y, 0)(a, b)
 
-    loop(a, 1)
-  }
+  def product(f: Int => Int)(a: Int, b: Int): Int = mapReduce(f, (x, y) => x * y, 1)(a, b)
 
   def fact(n: Int): Int = {
     product(x => x)(1, n)
   }
 
-  // TODO Write a function that generalizes sum and product
+  def mapReduce(f: Int => Int, combine: (Int, Int) => Int, zero: Int)(a: Int, b: Int): Int = {
+    def loop(a: Int, acc: Int): Int = {
+      if (a > b) acc
+      else loop(a + 1, combine(f(a), acc))
+    }
+
+    loop(a, zero)
+  }
+
 }
